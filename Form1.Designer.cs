592836f1138d0c3e1750a8c400831d8799f17591
danim <danim@SKYWALKER>
2024-01-10 11:58:50 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HolaMundoVideojuego
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(267, 292);
+            label1.Name = "label1";
+            label1.Size = new Size(235, 48);
+            label1.TabIndex = 0;
+            label1.Text = "Graficación y Videojuegos";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.BackColor = SystemColors.ActiveCaption;
+            label2.Font = new Font("Showcard Gothic", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(165, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(461, 53);
+            label2.TabIndex = 1;
+            label2.Text = "Daniel Martínez Maimone";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.BackColor = SystemColors.ActiveCaption;
+            label3.Font = new Font("Stencil", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(217, 229);
+            label3.Name = "label3";
+            label3.Size = new Size(343, 25);
+            label3.TabIndex = 2;
+            label3.Text = "ID: 173825";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "HolaMundoVideojuego";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
